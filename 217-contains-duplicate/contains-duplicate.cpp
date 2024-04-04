ade6b4acfacbf5a2c541f419caf97a830433dae5
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2024-04-04 16:34:00 +0300

    Time: 142 ms (6.21%) | Memory: 73.1 MB (63.92%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+       map <int,int> m;
+        
+        for(int i=0;i<nums.size();i++)
+            
+        {
+            if(m[nums[i]]==1)
+                return true;               //the moment it finds duplicate, returns true.
+            
+            m[nums[i]]=1;
+        }
+        return false;
+    
+    }
+    
+};
+
