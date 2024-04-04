9f03c68421d7c21ef7b14038f732cafffa2e123f
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2024-04-04 16:49:43 +0300

    Time: 3 ms (98.68%) | Memory: 14.1 MB (17.08%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
+
         //Key is the number and value is its index in the vector.
 	unordered_map<int, int> hash;
 	vector<int> result;
 	for (int i = 0; i < numbers.size(); i++) {
+
 		int numberToFind = target - numbers[i];
 
             //if numberToFind is found in map, return them
@@ -18,6 +20,7 @@ public:
             //number was not found. Put it in the map.
 		hash[numbers[i]] = i;
 	}
+    
 	return result;
     }
 };
