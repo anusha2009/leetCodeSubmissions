bbb9e9357637b629e7cdec91e7eecb6ac52d75de
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2024-11-05 18:12:02 +0100

    Time: 3 ms (79.15%) | Memory: 16 MB (7.98%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-
-        //Key is the number and value is its index in the vector.
-	unordered_map<int, int> hash;
-	vector<int> result;
-	for (int i = 0; i < numbers.size(); i++) {
-
-		int numberToFind = target - numbers[i];
-
-            //if numberToFind is found in map, return them
-		if (hash.find(numberToFind) != hash.end()) {
-                    //+1 because indices are NOT zero based
-			result.push_back(hash[numberToFind]);
-			result.push_back(i);			
-			return result;
-		}
-
-            //number was not found. Put it in the map.
-		hash[numbers[i]] = i;
-	}
-    
-	return result;
+    vector<int> twoSum(vector<int> &nums, int target) {
+        unordered_map<int, int> hash;
+        for (int i = 0; i < nums.size(); i++) {
+            hash[nums[i]] = i;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (hash.find(complement) != hash.end() && hash[complement] != i) {
+                return {i, hash[complement]};
+            }
+        }
+        // If no valid pair is found, return an empty vector
+        return {};
     }
 };
