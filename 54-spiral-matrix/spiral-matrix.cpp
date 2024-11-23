6dc158b2d7c4e51ad9a179d63f7817ec4ee0c4fa
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2024-11-23 17:06:21 +0100

    Time: 0 ms (100.00%) | Memory: 8.4 MB (59.67%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<int> res(m * n);
+        int j = 0;
+        int l = 0, u = 0, d = m - 1, r = n - 1;
+        
+        while (l <= r && u <= d) {
+            // Traverse from left to right
+            if (l <= r && u <= d) {
+                for (int i = l; i <= r; i++) {
+                    res[j++] = matrix[u][i];
+                }
+                u++;
+            }
+            
+            // Traverse from top to bottom
+            if (l <= r && u <= d) {
+                for (int i = u; i <= d; i++) {
+                    res[j++] = matrix[i][r];
+                }
+                r--;
+            }
+            
+            // Traverse from right to left
+            if (l <= r && u <= d) {
+                for (int i = r; i >= l; i--) {
+                    res[j++] = matrix[d][i];
+                }
+                d--;
+            }
+            
+            // Traverse from bottom to top
+            if (l <= r && u <= d) {
+                for (int i = d; i >= u; i--) {
+                    res[j++] = matrix[i][l];
+                }
+                l++;
+            }
+        }
+        
+        return res;
+    }
+};
