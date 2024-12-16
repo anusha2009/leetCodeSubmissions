a52917d632d015ab18e6c5110265620cdf834e5f
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2024-12-16 15:55:05 +0100

    Time: 81 ms (52.48%) | Memory: 74.8 MB (22.01%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_map<int, int> map;
-        int maxLength=0;
-        int currLength=0;
-        for(int i=0;i<nums.size();i++){
-            map[nums[i]]++;
-        }
-        for(int i=0;i<nums.size();i++){
-            currLength=0;
-            int temp = nums[i];
-            if(!map[temp-1]){
-                while(map[temp]){
-                currLength++;
-                temp++;
-            }
-            }
-            if(currLength>maxLength){
-                maxLength = currLength;
+        unordered_set<int> num_set(nums.begin(), nums.end());
+        int longestStreak = 0;
+        for (int num : num_set) {
+            if (!num_set.count(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+                while (num_set.count(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+                longestStreak = max(longestStreak, currentStreak);
             }
         }
-        return maxLength;
-
-        
+        return longestStreak;
     }
 };
