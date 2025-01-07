220c2e632e603d5c02e80ab88518ecd6e9eb9b4d
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-01-07 14:36:17 +0100

    Time: 0 ms (100.00%) | Memory: 8.4 MB (48.29%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        return build(s) == build(t);
+    }
+    
+    string build(string s) {
+        string ans = "";
+        for (char c: s) {
+            if (c != '#') {
+                ans.push_back(c);
+            } else if (!ans.empty()) {
+                ans.pop_back();
+            }
+        }
+        
+        return ans;
+    }
+};
