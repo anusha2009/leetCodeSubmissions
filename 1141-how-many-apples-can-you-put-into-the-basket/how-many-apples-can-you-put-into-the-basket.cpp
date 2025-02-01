cff44c1453b9dc1be1983df867d704023fa3a7f6
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-02-01 21:10:04 +0100

    Time: 3 ms (38.79%) | Memory: 19.8 MB (6.06%) - LeetSync

---
+++ 1141-how-many-apples-can-you-put-into-the-basket/how-many-apples-can-you-put-into-the-basket.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+public:
+    int maxNumberOfApples(vector<int>& weight) {
+        int max_weight = 5000;
+        int curr_weight = 0;
+        int num_of_baskets = 0;
+        priority_queue<int,vector<int>,greater<int>> minHeap (weight.begin(), weight.end());
+        while(!minHeap.empty()){
+            if(curr_weight+minHeap.top()<=max_weight){
+                curr_weight+= minHeap.top(); minHeap.pop();
+                num_of_baskets++;
+            }
+            else return num_of_baskets;
+        }
+        return num_of_baskets;
+    }
+};
