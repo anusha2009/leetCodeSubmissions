77a58e3dc5148e058090d62869f5e34434650189
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-02-02 17:20:34 +0100

    Time: 10 ms (14.74%) | Memory: 9.7 MB (16.21%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+private:
+    int size;
+
+public:
+    int totalNQueens(int n) {
+        // declaring unordered sets
+        unordered_set<int> diagonals, antiDiagonals, cols;
+        size = n;
+        return backtrack(0, diagonals, antiDiagonals, cols);
+    }
+
+private:
+    int backtrack(int row, unordered_set<int>& diagonals,
+                  unordered_set<int>& antiDiagonals, unordered_set<int>& cols) {
+        // Base case - N queens have been placed
+        if (row == size) {
+            return 1;
+        }
+        int solutions = 0;
+        for (int col = 0; col < size; col++) {
+            int currDiagonal = row - col;
+            int currAntiDiagonal = row + col;
+            // If the queen is not placeable
+            if (cols.find(col) != cols.end() ||
+                diagonals.find(currDiagonal) != diagonals.end() ||
+                antiDiagonals.find(currAntiDiagonal) != antiDiagonals.end()) {
+                continue;
+            }
+            // "Add" the queen to the board
+            cols.insert(col);
+            diagonals.insert(currDiagonal);
+            antiDiagonals.insert(currAntiDiagonal);
+            // Move on to the next row with the updated board state
+            solutions += backtrack(row + 1, diagonals, antiDiagonals, cols);
+            // "Remove" the queen from the board since we have already
+            // explored all valid paths using the above function call
+            cols.erase(col);
+            diagonals.erase(currDiagonal);
+            antiDiagonals.erase(currAntiDiagonal);
+        }
+        return solutions;
+    }
+};
