fd8cd91b27383af150895646cd426e13a5ececea
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-02-04 10:08:42 +0100

    Time: 10 ms (41.29%) | Memory: 13.5 MB (8.24%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     int maxVowels(string s, int k) {
-        int left = 0;
-        int maxNum = INT_MIN;
-        int count = 0;
-        for(int i = 0; i<k; i++) {
-            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u') {
-                count++;
-            }
-        }
-        maxNum = max(maxNum, count);
-        for(int i = k; i<s.size(); i++) {
-            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u') {
-                count++;
-            }
-            if(s[i-k]=='a' || s[i-k]=='e' || s[i-k]=='i' || s[i-k]=='o' || s[i-k]=='u') {
-                count--;
-            }
-            maxNum = max(maxNum, count);
-        }
-        return maxNum;
+        unordered_set<char> vowels{'a', 'e', 'i', 'o', 'u'};
         
+        // Build the window of size k, count the number of vowels it contains.
+        int count = 0;
+        for (int i = 0; i < k; i++) {
+            count += vowels.count(s[i]);
+        } 
+        int answer = count;
+        
+        // Slide the window to the right, focus on the added character and the
+        // removed character and update "count". Record the largest "count".
+        for (int i = k; i < s.length(); i++) {
+            count += vowels.count(s[i]) - vowels.count(s[i - k]);
+            answer = max(answer, count);
+        }
+        
+        return answer;
     }
 };
