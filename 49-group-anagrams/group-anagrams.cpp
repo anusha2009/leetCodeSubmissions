81bb07599d479fd36d84fff13d3a6276e274f93e
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-02-05 16:28:13 +0100

    Time: 71 ms (10.21%) | Memory: 34.4 MB (8.57%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-       vector<vector<string>> res;
-		map<string,vector<string>> m;
-		for(auto& x:strs){
+        unordered_map<string, vector<string>> map;
+        vector<vector<string>> result;
+        for(auto& str : strs) {
+            // Build the frequency array
+            vector<int> freq(26, 0);
+            for (char c : str) {
+                freq[c - 'a']++;
+            }
+            
+            // Convert freq to a key, e.g. "2#0#1#..." for each of the 26 counts
+            // There's many ways; as long as it uniquely represents the freq
+            string key;
+            for (int count : freq) {
+                key += to_string(count) + "#";
+            }
+            map[key].push_back(str);
+        }
+        for(auto& m : map) {
+            result.push_back(m.second);
+        }
+        return result;
 
-			auto tem=x;
-			sort(tem.begin(),tem.end());
-			m[tem].push_back(x);
-		}
-		for(auto v:m){
-
-			res.push_back(v.second);
-		}
-        
-		return res; 
     }
 };
+
