ee3c89466def512d577026b76cb7632f01de7a53
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-02-12 14:41:07 +0100

    Time: 12 ms (16.04%) | Memory: 18.3 MB (40.55%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.cpp
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.cpp
@@ -1,53 +1,28 @@
-class UnionFind {
-public:
-    UnionFind(int size): root(size), rank(size), count(size) {
-        for(int i=0;i<size;i++) {
-            root[i]=i;
-            rank[i]=1;
-        }
-    }
-    int find(int x) {
-        if(x==root[x]) {
-            return x;
-        }
-        return root[x] = find(root[x]);
-    }
-    void unionSet(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-        if(rootX != rootY) {
-            if(rank[rootX]>rank[rootY]) {
-                root[rootY] = rootX;
-            } else if(rank[rootY]>rank[rootX]) {
-                root[rootX] = rootY;
-            } else {
-                root[rootY] = rootX;
-                rank[rootX] += 1;
-            }
-            count--;
-        }
-
-    }
-    int getCount() {
-        return count;
-    }
-private:
-    vector<int> root;
-    vector<int> rank;
-    int count;
-};
-
 class Solution {
 public:
     int countComponents(int n, vector<vector<int>>& edges) {
-        if(n==0 || (edges.size()==0)) {
-            return 0;
+        unordered_map<int, vector<int>> graph;
+        vector<bool> seen(n, false);
+        for(auto& edge: edges) {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
         }
-        UnionFind uf(n);
-        for(int i=0;i<edges.size();i++) {
-            uf.unionSet(edges[i][0], edges[i][1]);
+        int count = 0;
+        for(int i=0;i<n;i++) {
+            if(!seen[i]) {
+                count++;
+                dfs(i, graph, seen);
+            }
         }
-        return uf.getCount();
+        return count;
         
+    }
+    void dfs(int source, unordered_map<int, vector<int>>& graph, vector<bool>& seen) {
+            seen[source] = true;
+            for(auto& neighbour : graph[source]) {
+                if(!seen[neighbour]) {
+                    dfs(neighbour, graph, seen);
+                }
+            }
     }
 };
