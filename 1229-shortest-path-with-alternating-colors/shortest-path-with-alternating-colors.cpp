be8743b44d8b85b3561c3a551a8b47bfe9add1e7
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-02-13 12:30:48 +0100

    Time: 7 ms (36.50%) | Memory: 20.2 MB (11.99%) - LeetSync

---
+++ 1229-shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges, vector<vector<int>>& blueEdges) {
+        constexpr int RED = 0;
+        constexpr int BLUE = 1;
+        
+        unordered_map<int, unordered_map<int, vector<int>>> graph;
+        for (vector<int>& edge: redEdges) {
+            int x = edge[0], y = edge[1];
+            graph[RED][x].push_back(y);
+        }
+        
+        for (vector<int> edge: blueEdges) {
+            int x = edge[0], y = edge[1];
+            graph[BLUE][x].push_back(y);
+        }
+        
+        vector<int> ans(n, INT_MAX);
+        queue<vector<int>> queue;
+        vector<vector<bool>> seen(n, vector(2, false));
+        
+        queue.push({0, RED, 0});
+        queue.push({0, BLUE, 0});
+        seen[0][RED] = true;
+        seen[0][BLUE] = true;
+        
+        while (!queue.empty()) {
+            vector<int> curr = queue.front();
+            queue.pop();
+            int node = curr[0], color = curr[1], steps = curr[2];
+            ans[node] = min(ans[node], steps);
+            
+            for (int neighbor: graph[color][node]) {
+                if (!seen[neighbor][1 - color]) {
+                    seen[neighbor][1 - color] = true;
+                    queue.push({neighbor, 1 - color, steps + 1});
+                }
+            }
+        }
+        
+        replace(ans.begin(), ans.end(), INT_MAX, -1);
+        
+        return ans;
+    }
+};
