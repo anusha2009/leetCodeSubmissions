effd96e8d0449ca367eb72e99e1d3ed47192a9a5
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-02-18 11:42:13 +0100

    Time: 3 ms (75.43%) | Memory: 18.6 MB (49.30%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,65 +1,40 @@
 class Solution {
 public:
-    int WHITE = 1;
-    int GRAY = 2;
-    int BLACK = 3;
-
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        bool isPossible = true;
-        map<int, int> color;
-        map<int, vector<int>> adjList;
-        vector<int> topologicalOrder;
+        vector<vector<int>> graph(numCourses);
+        vector<int> indegree(numCourses);
 
-        // By default all vertices are WHITE.
-        for (int i = 0; i < numCourses; i++) color[i] = WHITE;
-
-        // Create the adjacency list representation of the graph.
-        for (vector<int> relation : prerequisites) {
-            int dest = relation[0];
-            int src = relation[1];
-            // if the key doesn't exist in the adjList, std::map will
-            // automatically create a pair (key, vector()) and add it to the
-            // map.
-            adjList[src].push_back(dest);
+        for (auto p : prerequisites) {
+            int from = p[1];
+            int to = p[0];
+            graph[from].push_back(to);
+            ++indegree[to];
         }
 
-        // If the node is unprocessed, then call dfs on it.
-        for (int i = 0; i < numCourses && isPossible; i++) {
-            if (color[i] == WHITE) {
-                dfs(i, color, adjList, isPossible, topologicalOrder);
+        queue<int> zeroDegree;
+        for (int i = 0; i < indegree.size(); i++) {
+            if (indegree[i] == 0) {
+                zeroDegree.push(i);
             }
         }
 
-        vector<int> order;
-        if (isPossible) {
-            order.resize(numCourses);
-            for (int i = 0; i < numCourses; i++) {
-                order[i] = topologicalOrder[numCourses - i - 1];
-            }
-        }
-        return order;
-    }
-
-    void dfs(int node, map<int, int>& color, map<int, vector<int>>& adjList,
-             bool& isPossible, vector<int>& topologicalOrder) {
-        // Don't recurse further if we found a cycle already.
-        if (!isPossible) return;
-
-        // Start the recursion.
-        color[node] = GRAY;
-
-        // Traverse on neighboring vertices.
-        for (int neighbor : adjList[node]) {
-            if (color[neighbor] == WHITE) {
-                dfs(neighbor, color, adjList, isPossible, topologicalOrder);
-            } else if (color[neighbor] == GRAY) {
-                // An edge to a GRAY vertex represents a cycle.
-                isPossible = false;
+        vector<int> result;
+        while (!zeroDegree.empty()) {
+            int course = zeroDegree.front();
+            zeroDegree.pop();
+            result.push_back(course);
+            for (int child : graph[course]) {
+                --indegree[child];
+                if (indegree[child] == 0) {
+                    zeroDegree.push(child);
+                }
             }
         }
 
-        // Recursion ends. We mark it as black.
-        color[node] = BLACK;
-        topologicalOrder.push_back(node);
+        if(result.size() != numCourses){
+            return {};
+        }
+
+        return result;
     }
 };
