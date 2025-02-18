f1f33cff46ce9ac77e923ee265e49e5ff075463e
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-02-18 14:45:51 +0100

    Time: 4 ms (87.75%) | Memory: 31.3 MB (53.68%) - LeetSync

--- 1101-parallel-courses/parallel-courses.cpp
+++ 1101-parallel-courses/parallel-courses.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
     int minimumSemesters(int N, vector<vector<int>>& relations) {
-        vector<vector<int>> adjList(N+1);
-        vector<int> inDegree(N+1, 0);
-        vector<int> q;
-        int semester = 0;
-        int courses = 0;
-        for(auto& relation : relations) {
-            int source = relation[0];
-            int dest = relation[1];
-            adjList[source].push_back(dest);
-            inDegree[dest]++;
+        vector<int> inCount(N + 1, 0);  // or indegree
+        vector<vector<int>> graph(N + 1);
+        for (auto& relation : relations) {
+            graph[relation[0]].push_back(relation[1]);
+            inCount[relation[1]]++;
         }
-        for(int i = 0;i<inDegree.size(); i++) {
-            if(inDegree[i]==0) {
-                q.push_back(i);
+        int step = 0;
+        int studiedCount = 0;
+        vector<int> bfsQueue;
+        for (int node = 1; node < N + 1; node++) {
+            if (inCount[node] == 0) {
+                bfsQueue.push_back(node);
             }
         }
-        while(!q.empty()) {
-            semester++;
+        // start learning with BFS
+        while (!bfsQueue.empty()) {
+            // start new semester
+            step++;
             vector<int> nextQueue;
-            for (auto& node : q) {
-                courses++;
-                for (auto& endNode : adjList[node]) {
-                    inDegree[endNode]--;
+            for (auto& node : bfsQueue) {
+                studiedCount++;
+                for (auto& endNode : graph[node]) {
+                    inCount[endNode]--;
                     // if all prerequisite courses learned
-                    if (inDegree[endNode] == 0) {
+                    if (inCount[endNode] == 0) {
                         nextQueue.push_back(endNode);
                     }
                 }
             }
-            q = nextQueue;
+            bfsQueue = nextQueue;
         }
-        return courses == N+1? semester:-1;
 
+        // check if learn all courses
+        return studiedCount == N ? step : -1;
     }
 };
