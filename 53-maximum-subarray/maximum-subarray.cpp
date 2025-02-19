925b090017acd498e3b5d76c30e1201b28d60a12
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-02-19 20:41:09 +0100

    Time: 0 ms (100.00%) | Memory: 71.7 MB (52.68%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-    int res = nums[0];
-    int maxEnding = nums[0];
-
-    for (int i = 1; i < nums.size(); i++) {
-      
-        // Find the maximum sum ending at index i by either extending 
-        // the maximum sum subarray ending at index i - 1 or by
-        // starting a new subarray from index i
-        maxEnding = max(maxEnding + nums[i], nums[i]);
-      
-        // Update res if maximum subarray sum ending at index i > res
-        res = max(res, maxEnding);
-    }
-    return res;
+        int maxSub = nums[0], curSum = 0;
+        for (int num : nums) {
+            if (curSum < 0) {
+                curSum = 0;
+            }
+            curSum += num;
+            maxSub = max(maxSub, curSum);
+        }
+        return maxSub;
     }
 };
