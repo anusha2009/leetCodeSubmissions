fd16839227398e50c243958e937c548e694615b0
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-02-22 17:07:21 +0100

    Time: 0 ms (100.00%) | Memory: 20.7 MB (59.01%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int singleNumber(vector<int>& a) {
+        
         //xor all numbers, the left over number would be the non repeated one
-     // since the equl numbers cancel out each others bits
+     // since the equal numbers cancel out each others bits
      int num = 0;
      for (int i = 0; i < a.size(); ++i) {
-        
+
          num ^= a[i];
      }
      return num;
