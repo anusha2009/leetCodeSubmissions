8ff826fcb33390e1b9de620d2d8078f5f6c5b0cd
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-03-02 17:39:21 +0100

    Time: 11 ms (11.52%) | Memory: 9.6 MB (16.12%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.cpp
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -1,44 +1,48 @@
 class Solution {
-private:
-    int size;
-
 public:
+    int n;
+    
     int totalNQueens(int n) {
-        // declaring unordered sets
-        unordered_set<int> diagonals, antiDiagonals, cols;
-        size = n;
+        this->n = n;
+        unordered_set<int> diagonals;
+        unordered_set<int> antiDiagonals;
+        unordered_set<int> cols;
         return backtrack(0, diagonals, antiDiagonals, cols);
     }
-
-private:
-    int backtrack(int row, unordered_set<int>& diagonals,
-                  unordered_set<int>& antiDiagonals, unordered_set<int>& cols) {
+    
+    int backtrack(int row, unordered_set<int>& diagonals, unordered_set<int>& antiDiagonals, unordered_set<int>& cols) {
         // Base case - N queens have been placed
-        if (row == size) {
+        if (row == n) {
             return 1;
         }
+        
         int solutions = 0;
-        for (int col = 0; col < size; col++) {
+        for (int col = 0; col < n; col++) {
             int currDiagonal = row - col;
             int currAntiDiagonal = row + col;
+            
             // If the queen is not placeable
             if (cols.find(col) != cols.end() ||
-                diagonals.find(currDiagonal) != diagonals.end() ||
-                antiDiagonals.find(currAntiDiagonal) != antiDiagonals.end()) {
+               diagonals.find(currDiagonal) != diagonals.end() ||
+               antiDiagonals.find(currAntiDiagonal) != antiDiagonals.end()) {
                 continue;
             }
+            
             // "Add" the queen to the board
             cols.insert(col);
             diagonals.insert(currDiagonal);
             antiDiagonals.insert(currAntiDiagonal);
+            
             // Move on to the next row with the updated board state
             solutions += backtrack(row + 1, diagonals, antiDiagonals, cols);
+            
             // "Remove" the queen from the board since we have already
             // explored all valid paths using the above function call
             cols.erase(col);
             diagonals.erase(currDiagonal);
             antiDiagonals.erase(currAntiDiagonal);
         }
+        
         return solutions;
     }
 };
