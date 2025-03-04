a04d522be6d0eca76652b948d6dc404727be8e30
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-03-04 15:28:55 +0100

    Time: 8 ms (77.80%) | Memory: 22.8 MB (29.37%) - LeetSync

---
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<int>> memo;
+    vector<vector<int>> matrix;
+    int m;
+    int n;
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        this->matrix = matrix;
+        this->m = matrix.size();
+        this->n = matrix[0].size();
+        memo = vector(m, vector<int>(n, -1));
+        int maxPath = INT_MIN;
+        for(int i = 0; i<m; i++) {
+            for(int j = 0; j<n; j++) {
+                maxPath = max(maxPath, dp(i, j));
+            }
+        }
+        return maxPath;
+    }
+    int dp(int i, int j) {
+        if(i>=m || j>=n) {
+            return 0;
+        }
+        if(memo[i][j]!=-1) {
+            return memo[i][j];
+        }
+        int ans = 0;
+        if(j>=1 && matrix[i][j]<matrix[i][j-1]) {
+            ans = max(ans, dp(i, j-1));
+        } 
+        if(j<n-1 && matrix[i][j]<matrix[i][j+1]) {
+            ans = max(ans, dp(i, j+1));
+        }
+        if(i<m-1 && matrix[i][j]<matrix[i+1][j]) {
+            ans = max(ans, dp(i+1, j));
+        }
+        if(i>=1 && matrix[i][j]<matrix[i-1][j]) {
+            ans = max(ans, dp(i-1, j));
+        }
+        memo[i][j] = ans + 1;
+        return memo[i][j];
+    }
+};
