d0405de90598e68578a4d9b561a8449951a9cf02
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-03-10 20:08:09 +0100

    Time: 0 ms (100.00%) | Memory: 20.5 MB (61.75%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,5 +1,33 @@
 class Solution {
 public:
+    int jump(vector<int>& nums) {
+        // The starting range of the first jump is [0, 0]
+        int answer = 0, n = int(nums.size());
+
+        int curEnd = 0, curFar = 0;
+
+        for (int i = 0; i < n - 1; ++i) {
+            // Update the farthest reachable index of this jump.
+            curFar = max(curFar, i + nums[i]);
+
+            // If we finish the starting range of this jump,
+            // Move on to the starting range of the next jump.
+            if (i == curEnd) {
+                answer++;
+                curEnd = curFar;
+            }
+        }
+
+        return answer;
+    }
+};
+
+// DP - Top Down
+
+/*
+
+class Solution {
+public:
     vector<int> memo;
     vector<int> nums;
     int n;
@@ -24,3 +52,5 @@ public:
         return memo[i];  // If no jump works, return false
     }
 };
+
+*/
