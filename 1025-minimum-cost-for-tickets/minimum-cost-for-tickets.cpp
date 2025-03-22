38def2eb7823fce6b2895257acea12f2fd680e2c
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-03-22 16:10:31 +0100

    Time: 3 ms (26.01%) | Memory: 13.2 MB (68.70%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,35 +1,41 @@
 class Solution {
 public:
-    vector<int> memo;
-    vector<int> days;
-    vector<int> costs;
-
-    int mincostTickets(vector<int>& days, vector<int>& costs) {
-        this->days = days;
-        this->costs = costs;
-        memo = vector<int>(days.size(), -1);
-        return dp(0);
+    unordered_set<int> isTravelNeeded;
+    
+    int solve(vector<int>& dp, vector<int>& days, vector<int>& costs, int currDay) {
+        // If we have iterated over travel days, return 0.
+        if (currDay > days[days.size() - 1]) {
+            return 0;
+        }
+        
+        // If we don't need to travel on this day, move on to next day.
+        if (isTravelNeeded.find(currDay) == isTravelNeeded.end()) {
+            return solve(dp, days, costs, currDay + 1);
+        }
+        
+        // If already calculated, return from here with the stored answer.
+        if (dp[currDay] != -1) {
+            return dp[currDay];
+        }
+        
+        int oneDay = costs[0] + solve(dp, days, costs, currDay + 1);
+        int sevenDay = costs[1] + solve(dp, days, costs, currDay + 7);
+        int thirtyDay = costs[2] + solve(dp, days, costs, currDay + 30);
+        
+        // Store the cost with the minimum of the three options.
+        return dp[currDay] = min(oneDay, min(sevenDay, thirtyDay));
     }
-
-    int dp(int i) {
-        if (i >= days.size()) return 0;
-        if (memo[i] != -1) return memo[i];
-
-        // Option 1: 1-day pass
-        int j = i;
-        while (j < days.size() && days[j] < days[i] + 1) j++;
-        int cost1 = costs[0] + dp(j);
-
-        // Option 2: 7-day pass
-        j = i;
-        while (j < days.size() && days[j] < days[i] + 7) j++;
-        int cost7 = costs[1] + dp(j);
-
-        // Option 3: 30-day pass
-        j = i;
-        while (j < days.size() && days[j] < days[i] + 30) j++;
-        int cost30 = costs[2] + dp(j);
-
-        return memo[i] = min({cost1, cost7, cost30});
+    
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        // The last day on which we need to travel.
+        int lastDay = days[days.size() - 1];
+        vector<int> dp(lastDay + 1, -1);
+        
+        // Mark the days on which we need to travel.
+        for (int day : days) {
+            isTravelNeeded.insert(day);
+        }
+        
+        return solve(dp, days, costs, 1);
     }
 };
