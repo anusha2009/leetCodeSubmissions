833e3d5e1530392de7cb7838c322e8f28fb4440a
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-03-24 13:57:34 +0100

    Time: 0 ms (100.00%) | Memory: 24.1 MB (74.25%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -1,30 +1,12 @@
 class Solution {
 public:
-    vector<int> memo;
-    vector<int> flowerbed;
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        this->flowerbed = flowerbed;
-        memo = vector<int>(flowerbed.size(), -1);
-        return dp(0, n) == 1;
-    }
-    int dp(int i, int n) {
-        if(n==0) {
-            return true;
+        for(int i = 0; i<flowerbed.size(); i++) {
+            if(flowerbed[i] == 0 && (i==0 || flowerbed[i-1] == 0 ) && (i==flowerbed.size()-1 || flowerbed[i+1] == 0)) {
+                flowerbed[i] = 1;
+                n--;
+            }
         }
-        if(i>=flowerbed.size()) {
-            return false;
-        }
-        if(memo[i]!=-1) {
-            return memo[i];
-        }
-        int ans;
-        if(flowerbed[i] == 0 && (i==0 || flowerbed[i-1] == 0 ) && (i==flowerbed.size()-1 || flowerbed[i+1] == 0)) {
-            flowerbed[i] = 1;
-            ans = dp(i+1, n-1);
-        } else {
-            ans = dp(i+1, n);
-        }
-        memo[i] = ans;
-        return ans;
+        return n <= 0;
     }
 };
