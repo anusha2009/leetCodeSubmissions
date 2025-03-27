94604f81abc8eca172f0048188f2801da1f0a729
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-03-27 21:07:45 +0100

    Time: 0 ms (100.00%) | Memory: 19.6 MB (97.66%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,13 +1,44 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int, int> map;
-        vector<int> result;
+        int count1 = 0;
+        int count2 = 0;
+        int candidate1 = INT_MIN;
+        int candidate2 = INT_MIN;
         for(int num : nums) {
-            map[num]++;
-            if(map[num] > nums.size()/3 && find(result.begin(), result.end(), num) == result.end()) {
-                result.push_back(num);
+            if(candidate1!=INT_MIN && candidate1 == num) {
+                count1++;
+            } else if(candidate2!=INT_MIN && candidate2 == num) {
+                count2++;
+            } else if(count1 == 0) {
+                candidate1 = num;
+                count1++;
+            } else if(count2 == 0) {
+                candidate2 = num;
+                count2++;
+            } else {
+                count1--;
+                count2--;
             }
+        }
+
+        vector<int> result;
+        count1 = 0;
+        count2 = 0;
+        for(int num : nums) {
+            if(candidate1!=INT_MIN && num == candidate1) {
+                count1++;
+            }
+            if(candidate2!=INT_MIN && num == candidate2) {
+                count2++;
+            }
+        }
+        int n = nums.size();
+        if(count1 > n/3) {
+            result.push_back(candidate1);
+        }
+        if(count2 > n/3) {
+            result.push_back(candidate2);
         }
         return result;
     }
