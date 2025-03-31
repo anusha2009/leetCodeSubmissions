ba4cb652f2778ef44b66cc14ebf9843e07938f62
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-03-31 17:02:30 +0200

    Time: 47 ms (76.38%) | Memory: 13.5 MB (97.53%) - LeetSync

--- 277-find-the-celebrity/find-the-celebrity.cpp
+++ 277-find-the-celebrity/find-the-celebrity.cpp
@@ -4,36 +4,26 @@
 class Solution {
 public:
     int findCelebrity(int n) {
-        vector<vector<int>> adjList(n);
-        set<int> candidates;
-        set<int> invalidCandidates;
-        int result = -1;
+        int celeb = 0;
         for(int i = 0; i<n; i++) {
-            for(int j = 0; j<n; j++) {
-                if(i!=j && knows(i, j)) {
-                    adjList[i].push_back(j);
-                }
-            }
-        }
-        for(int i = 0; i<adjList.size(); i++) {
-            if(adjList[i].empty()) {
-                cout<<i<<'\n';
-                candidates.insert(i);
+            if(knows(celeb, i)) {
+                celeb = i;
             }
         }
 
-        for(int node : candidates) {
-            for(int i = 0; i<adjList.size(); i++) {
-                if(i != node && find(adjList[i].begin(), adjList[i].end(), node) == adjList[i].end()) {
-                    invalidCandidates.insert(node);
-                }
+        if(isCeleb(celeb, n)) {
+            return celeb;
+        }
+        return -1;
+    }
+
+    int isCeleb(int celeb, int n) {
+        for(int i = 0; i<n; i++) {
+            if(celeb==i) continue;
+            if(knows(celeb, i) || !knows(i, celeb)) {
+                return false;
             }
         }
-        for(int node : candidates) {
-            if(find(invalidCandidates.begin(), invalidCandidates.end(), node) == invalidCandidates.end()) {
-                result = node;
-            }
-        }
-        return result;
+        return true;
     }
 };
