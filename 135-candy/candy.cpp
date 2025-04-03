0fe09993a5d4eb9b4e20bdfeed58dcddb2b88434
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-04-03 12:03:07 +0200

    Time: 0 ms (100.00%) | Memory: 22 MB (95.01%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int candy(vector<int>& ratings) {
         vector<int> candies(ratings.size(), 1);
@@ -15,5 +15,48 @@ public:
             sum += candies[i];
         }
         return sum;
+    }
+};*/
+
+class Solution {
+public:
+    // Function to calculate sum of first n natural numbers
+    int count(int n) { return (n * (n + 1)) / 2; }
+    int candy(vector<int>& ratings) {
+        if (ratings.size() <= 1) {
+            return ratings.size();
+        }
+        int candies = 0;
+        int up = 0;
+        int down = 0;
+        int oldSlope = 0;
+        for (int i = 1; i < ratings.size(); i++) {
+            int newSlope = (ratings[i] > ratings[i - 1])
+                               ? 1
+                               : (ratings[i] < ratings[i - 1] ? -1 : 0);
+            // slope is changing from uphill to flat or downhill
+            // or from downhill to flat or uphill
+            if ((oldSlope > 0 && newSlope == 0) ||
+                (oldSlope < 0 && newSlope >= 0)) {
+                candies += count(up) + count(down) + max(up, down);
+                up = 0;
+                down = 0;
+            }
+            // slope is uphill
+            if (newSlope > 0) {
+                up++;
+            }
+            // slope is downhill
+            else if (newSlope < 0) {
+                down++;
+            }
+            // slope is flat
+            else {
+                candies++;
+            }
+            oldSlope = newSlope;
+        }
+        candies += count(up) + count(down) + max(up, down) + 1;
+        return candies;
     }
 };
