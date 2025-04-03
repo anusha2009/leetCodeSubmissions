fcb0f5bf589194bf670a349bbead3c7b8020a814
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-04-03 11:21:33 +0200

    Time: 0 ms (100.00%) | Memory: 40.2 MB (79.66%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> lhs(nums.size(), 1);
-        int rhs = 1;
-        for(int i = 1; i<nums.size() ; i++){
-            lhs[i] = lhs[i-1] * nums[i-1];
+        // The length of the input array
+        int length = nums.size();
+
+        // The answer vector to be returned
+        vector<int> answer(length);
+
+        // answer[i] contains the product of all the elements to the left
+        // Note: for the element at index '0', there are no elements to the
+        // left, so answer[0] would be 1
+        answer[0] = 1;
+        for (int i = 1; i < length; i++) {
+            // answer[i - 1] already contains the product of elements to the
+            // left of 'i - 1' Simply multiplying it with nums[i - 1] would give
+            // the product of all elements to the left of index 'i'
+            answer[i] = nums[i - 1] * answer[i - 1];
         }
-        for(int i = lhs.size()-1; i >= 1 ; i--){
-            rhs *= nums[i];
-            lhs[i-1] *= rhs;
+
+        // R contains the product of all the elements to the right
+        // Note: for the element at index 'length - 1', there are no elements to
+        // the right, so R would be 1
+        int R = 1;
+        for (int i = length - 1; i >= 0; i--) {
+            // For the index 'i', R would contain the
+            // product of all elements to the right. We update R accordingly
+            answer[i] = answer[i] * R;
+            R *= nums[i];
         }
-        
-        return lhs;
-    
+
+        return answer;
     }
 };
