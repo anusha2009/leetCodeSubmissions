b927d18e2024a91f10f149aff1748d18d1220bc9
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-04-18 21:32:59 +0200

    Time: 0 ms (100.00%) | Memory: 31.5 MB (16.53%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -11,9 +11,50 @@
  */
 class Solution {
 public:
+    bool exists(int idx, int d, TreeNode* node) {
+        int left = 0;
+        int right = pow(2, d) - 1;
+        int pivot;
+        for(int i = 0; i<d; i++) {
+            pivot = left + (right - left)/2;
+            if(idx <= pivot) {
+                node = node->left;
+                right = pivot;
+            } else {
+                node = node->right;
+                left = pivot+1;
+            }
+        }
+        return node != nullptr;
+    }
+    int computeDepth(TreeNode* node) {
+        int d = 0;
+        while(node->left != nullptr) {
+            node = node->left;
+            d++;
+        }
+        return d;
+    }
     int countNodes(TreeNode* root) {
-        return root != nullptr ? 1 + countNodes(root->right) + countNodes(root->left) : 0;
-        
+        if(root == nullptr) {
+            return 0;
+        }
+        int d = computeDepth(root);
+        if(d == 0) {
+            return 1;
+        }
+        int left = 1;
+        int right = pow(2, d) - 1;
+        int pivot;
+        while(left <= right) {
+            pivot = left + (right - left)/2;
+            if(exists(pivot, d, root)) {
+                left = pivot + 1;
+            } else {
+                right = pivot - 1;
+            }
+        }
+        return pow(2, d) - 1 + left;
     }
 
 };
