e0cb378b0b5b4537f3b954331764fc7b8157de4e
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-04-21 21:23:51 +0200

    Time: 23 ms (30.37%) | Memory: 37 MB (94.76%) - LeetSync

--- 3825-apply-substitutions/apply-substitutions.cpp
+++ 3825-apply-substitutions/apply-substitutions.cpp
@@ -1,43 +1,54 @@
 class Solution {
 public:
-    string applySubstitutions(vector<vector<string>>& replacements, string text) {
-        for(auto& replacement : replacements)
-            dict[replacement[0]] = replacement[1];
-        for(auto& [key, value] : dict)
-            dfs(key);
-        int tIndex = 0, tLen = text.size();
-        string res;
-        while(tIndex < tLen){
-            if(text[tIndex] == '%'){
-                string key(1, text[tIndex + 1]);
-                res += dict[key];
-                tIndex += 3;
-            }
-            else
-                res.push_back(text[tIndex++]);
+    void dfs(unordered_map<char, bool>& visited, char currentPlaceholder, unordered_map<char, bool>& placeholders, unordered_map<char, string>& replacement_map, string& ret) {
+        if (visited[currentPlaceholder]) return;
+        if (currentPlaceholder == '_') {
+            ret.push_back('_');
+            return;
         }
-        return res;
+        visited[currentPlaceholder] = true;
+        // if the current placeholder has another placeholder in its replacements
+        // go dfs to see that placeholder's replacements
+        string replacement = replacement_map[currentPlaceholder];
+        for (auto& c : replacement) {
+            if (replacement_map.find(c) != replacement_map.end()) {
+                dfs(visited, c, placeholders, replacement_map, ret);
+            }
+            else if (c != '%') {
+                ret.push_back(c);
+            } else if (c == '%') {
+                continue;
+            }
+        }
+        visited[currentPlaceholder] = false;
     }
-private:
-    unordered_map<string, string> dict;
-    unordered_set<string> visited;
-    string dfs(string key){
-        if(visited.find(key) != visited.end())
-            return dict[key];
-        string replacement = dict[key];
-        string curRes;
-        int index = 0;
-        while(index < replacement.size()){
-            if(replacement[index] == '%'){
-                string curKey(1, replacement[index + 1]);
-                curRes += dfs(curKey);
-                index += 3;
-            }
-            else
-                curRes.push_back(replacement[index++]);
+    string applySubstitutions(vector<vector<string>>& replacements, string text) {
+        unordered_map<char, bool> visited;
+        // if the replacements contain the placeholder, we need to check if we can form the placeholder
+        // build the placeholder list
+        unordered_map<char, bool> placeholders;
+        unordered_map<char, string> replacement_map;
+        string ret;
+        string currentPlaceholder;
+        for (int i = 0; i < replacements.size(); ++i) {
+            char first = replacements[i][0][0];
+            string second = replacements[i][1];
+            replacement_map[first] = second;
         }
-        dict[key] = curRes;
-        visited.insert(key);
-        return curRes;
-    } 
+        for (int i = 0; i < text.size(); ++i) {
+            // mark the placeholder in map == true
+            char curr = text[i];
+            if (curr == '_') {
+                currentPlaceholder.push_back('_');
+            } 
+            else if (curr != '%') {
+                placeholders[text[i]] = true;
+                currentPlaceholder.push_back(text[i]);
+            }
+        }
+        for (auto& c : currentPlaceholder) {
+            dfs(visited, c, placeholders, replacement_map, ret);
+        }
+        return ret;
+    }
 };
