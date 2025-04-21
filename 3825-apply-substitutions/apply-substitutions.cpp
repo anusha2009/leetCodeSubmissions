12b6101d0c5a7f33d3b182eff3d27f585a8d8e8b
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-04-21 21:45:12 +0200

    Time: 11 ms (73.07%) | Memory: 36.9 MB (94.85%) - LeetSync

--- 3825-apply-substitutions/apply-substitutions.cpp
+++ 3825-apply-substitutions/apply-substitutions.cpp
@@ -1,20 +1,54 @@
 class Solution {
 public:
-    string applySubstitutions(vector<vector<string>>& replacements, string text) {
-        unordered_map<string, string> replacementsMapping;
-
-        for(auto replacement: replacements) 
-            replacementsMapping[replacement[0]] = replacement[1];
-
-        for(int i = 0; i < text.size();) {
-            string key(1, text[i]);
-            if(text[i] == '%') text.erase(i, 1);
-            else if(replacementsMapping.find(key) != replacementsMapping.end()) {
-                text.erase(i, 1);
-                text.insert(i, replacementsMapping[key]);
-            } else ++i;
+    void dfs(unordered_map<char, bool>& visited, char currentPlaceholder, unordered_map<char, bool>& placeholders, unordered_map<char, string>& replacement_map, string& ret) {
+        if (visited[currentPlaceholder]) return;
+        if (currentPlaceholder == '_') {
+            ret.push_back('_');
+            return;
         }
-
-        return text;
+        visited[currentPlaceholder] = true;
+        // if the current placeholder has another placeholder in its replacements
+        // go dfs to see that placeholder's replacements
+        string replacement = replacement_map[currentPlaceholder];
+        for (auto& c : replacement) {
+            if (replacement_map.find(c) != replacement_map.end()) {
+                dfs(visited, c, placeholders, replacement_map, ret);
+            }
+            else if (c != '%') {
+                ret.push_back(c);
+            } else if (c == '%') {
+                continue;
+            }
+        }
+        visited[currentPlaceholder] = false;
+    }
+    string applySubstitutions(vector<vector<string>>& replacements, string text) {
+        unordered_map<char, bool> visited;
+        // if the replacements contain the placeholder, we need to check if we can form the placeholder
+        // build the placeholder list
+        unordered_map<char, bool> placeholders;
+        unordered_map<char, string> replacement_map;
+        string ret;
+        string currentPlaceholder;
+        for (int i = 0; i < replacements.size(); ++i) {
+            char first = replacements[i][0][0];
+            string second = replacements[i][1];
+            replacement_map[first] = second;
+        }
+        for (int i = 0; i < text.size(); ++i) {
+            // mark the placeholder in map == true
+            char curr = text[i];
+            if (curr == '_') {
+                currentPlaceholder.push_back('_');
+            } 
+            else if (curr != '%') {
+                placeholders[text[i]] = true;
+                currentPlaceholder.push_back(text[i]);
+            }
+        }
+        for (auto& c : currentPlaceholder) {
+            dfs(visited, c, placeholders, replacement_map, ret);
+        }
+        return ret;
     }
 };
