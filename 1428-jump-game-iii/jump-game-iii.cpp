dbb242aea978dbeaa0103aeb5cc22f52652f82d1
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-04-22 14:27:15 +0200

    Time: 0 ms (100.00%) | Memory: 35.6 MB (66.31%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        queue<pair<int, int>> q;
+        vector<bool> seen(arr.size(), 0);
+        q.push({start, arr[start]});
+        seen[start] = 1;
+        int steps = 0;
+        while(!q.empty()) {
+            auto curr = q.front();
+            q.pop();
+            int node = curr.first;
+            int val = curr.second;
+            if(val == 0) {
+                return true;
+            }
+            if(node + val < arr.size() && !seen[node+val]) {
+                seen[node+val] = 1;
+                q.push({node+val, arr[node+val]});
+            }
+            if(node - val >= 0 && !seen[node-val]) {
+                seen[node-val] = 1;
+                q.push({node-val, arr[node-val]});
+            }
+            steps++;
+        }
+        return false;
+
+    }
+};
