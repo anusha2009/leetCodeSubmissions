e2b4b1162748cd23d9e655aee5a99357ba05ed0e
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-04-24 14:34:35 +0200

    Time: 0 ms (100.00%) | Memory: 17.5 MB (18.02%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,6 +1,39 @@
 class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> color(n, -1); // -1 means uncolored
+
+        for (int start = 0; start < n; ++start) {
+            if (color[start] == -1) {
+                queue<int> q;
+                q.push(start);
+                color[start] = 0;
+
+                while (!q.empty()) {
+                    int node = q.front();
+                    q.pop();
+
+                    for (int neighbor : graph[node]) {
+                        if (color[neighbor] == -1) {
+                            color[neighbor] = 1 - color[node];
+                            q.push(neighbor);
+                        } else if (color[neighbor] == color[node]) {
+                            return false; // Same color on both sides -> not bipartite
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+};
+
+//DFS
+/*class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
         vector<vector<int>> adj;
         vector<bool> vis(graph.size(), false);
         vector<int> colour(graph.size(), -1);
@@ -27,4 +60,4 @@ public:
         }
         return true;
     }
-};
+};*/
