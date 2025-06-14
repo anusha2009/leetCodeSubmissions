373ba22a3bbc92f88ccdb6a80223e72d9791540d
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-06-14 11:29:34 +0200

    Time: 45 ms (21.46%) | Memory: 64.3 MB (74.32%) - LeetSync

--- 2681-put-marbles-in-bags/put-marbles-in-bags.cpp
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.cpp
@@ -5,7 +5,7 @@ public:
         int n = weights.size();
         vector<int> pairWeights(n - 1, 0);
         for (int i = 0; i < n - 1; ++i) {
-            pairWeights[i] += weights[i] + weights[i + 1];
+            pairWeights[i] = weights[i] + weights[i + 1];
         }
 
         sort(pairWeights.begin(), pairWeights.end());
