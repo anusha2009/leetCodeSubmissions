8396a38449aae8723fcb891d8b49465474003c8a
Anusha Annengala <52235081+anusha2009@users.noreply.github.com>
2025-06-14 15:56:20 +0200

    Time: 0 ms (100.00%) | Memory: 8.2 MB (47.34%) - LeetSync

--- 365-water-and-jug-problem/water-and-jug-problem.cpp
+++ 365-water-and-jug-problem/water-and-jug-problem.cpp
@@ -1,33 +1,43 @@
 class Solution {
 public:
-    bool canMeasureWater(int jug1Capacity, int jug2Capacity, int targetCapacity) {
-        
-        int x=jug1Capacity,y=jug2Capacity,z=x+y;
-        int steps[]={x,-x,-y,y}; //STEPS THAT CAN BE PERFORMED
-        
-        queue<int> q;
-        vector<int> vis(z+1,0); // VISITED ARRAY TO KEEP NOT ITERATING THE SAME VALUES OVER AND OVER AGAIN
-        q.push(0);
-        vis[0]=1;
-        while(!q.empty())
-        {
-            int node=q.front();
+    bool canMeasureWater(int x, int y, int target) {
+        if(target>x+y){
+            return false;
+        }
+        if(target==0){
+            return true;
+        }
+        if(x==0){
+            return y==target;
+        }
+        if(y==0){
+            return x==target;
+        }
+        queue<pair<int,int>>q;
+        set<pair<int,int>>visited;
+        q.push({0,0});
+        visited.insert({0,0}); 
+        while(!q.empty()){
+            auto [dx,dy]=q.front();
             q.pop();
-            
-            if(node==targetCapacity)
-            {
-                return true; // WHEN WE FIND THE TARGET CAPACITY ACHIEVED
+            if(dx+dy==target){
+                return true;
             }
-            for(int i=0;i<4;i++)
-            {
-                int newNode=node+steps[i];
-                if(newNode>=0 && newNode<=z && vis[newNode]==0) //BOUNDARY CHECKS 
-                {
-                    q.push(newNode);
-                    vis[newNode]=1;
+            vector<pair<int, int>> dirs = {
+                {x, dy},
+                {dx, y},
+                {0, dy},
+                {dx, 0}, 
+                {dx - min(dx, y - dy), dy + min(dx, y - dy)},
+                {dx + min(dy, x - dx), dy - min(dy, x - dx)} 
+            };
+            for(auto [nx,ny]:dirs){
+                if(visited.count({nx,ny})==0){
+                    visited.insert({nx,ny});
+                    q.push({nx,ny});
                 }
             }
         }
-        return false; // IF TARGET CAPACITY CAN NEVER BE ACHIVED
+        return false;
     }
 };
